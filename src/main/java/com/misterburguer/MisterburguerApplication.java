04ac34fb225e5c6f4b224dc3b6bd70875428d933
Darian Beluzzo <darian.beluzzo@gmail.com>
2019-03-30 15:09:05 -0300

    Criação do projeto

---
+++ src/main/java/com/misterburguer/MisterburguerApplication.java
@@ -0,0 +1,13 @@
+package com.misterburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisterburguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisterburguerApplication.class, args);
+	}
+
+}
