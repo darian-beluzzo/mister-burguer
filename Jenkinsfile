6f45c2af251f4be05630f57d6d389f21117cb325
Darian Beluzzo <darian.beluzzo@synchro.com.br>
2019-04-11 11:43:15 -0300

    Configuração do Jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,15 +1,10 @@
 
 
 pipeline {
-    script {
-        env.MAVEN_CONFIG = readFile 'maven.env'
-        env.MAVEN_CONFIG = ${env.MAVEN_CONFIG} - 'MAVEN_CONFIG='
-    }
-
     agent {
         docker {
             image 'maven:3.6-jdk-8' 
-            args "-v ${env.MAVEN_CONFIG}:/root/.m2' 
+            args "-v ${props.MAVEN_CONFIG}:/root/.m2' 
         }
     }
     stages {
--- docker/prod/build-run.sh
+++ docker/prod/build-run.sh
@@ -3,12 +3,22 @@
 FILE_PATH=$(test -L $0 && ls -l $0 | awk '{print $11;}' || echo "$0")
 REAL_PATH=`readlink -f $(dirname $FILE_PATH)` #Diretório onde está este script (full path)
 
-cd $REALPATH
+cd $REAL_PATH
+
+if [ -z "$MAVEN_CONFIG" ]; then
+	export MAVEN_CONFIG=~/.m2
+fi
 
 # Build do backend
-docker run -u $UID --rm -v $REAL_PATH/../../api:/app -v $HOME/.m2:/app/.m2 -e MAVEN_CONFIG=/app/.m2 -w /app --name api-build maven:3.6-jdk-8 mvn -Duser.home=/app package
+docker run -u $UID --rm -v $REAL_PATH/../../api:/app -v $MAVEN_CONFIG:/app/.m2 -e MAVEN_CONFIG=/app/.m2 -w /app --name api-build maven:3.6-jdk-8 mvn -Duser.home=/app package
 
 # Build do frontend
 docker run -u $UID --rm -v $REAL_PATH/../../frontend:/app -w /app --name react-build node /bin/bash -c "yarn; yarn build";
 
+# Inicia os servicos
 docker-compose up -d
+
+echo
+echo '---------------------------------------'
+echo 'Pronto! Acesse http://localhost'
+echo '---------------------------------------'
